1bae5391170f8eb1f99f051489d1f8f33ad9f3e6
admyazeed <89387525+admyazeed@users.noreply.github.com>
2026-05-27 18:11:32 +0200

    Add orthographic projection matrix

--- Assignment-2/a2-python/src/GLWindow.py
+++ Assignment-2/a2-python/src/GLWindow.py
@@ -28,6 +28,18 @@ def rotate(theta):
     )
 
 
+def orthographic(left, right, bottom, top, near, far):
+    return np.array(
+        [
+            [2 / (right - left), 0, 0, -(right + left) / (right - left)],
+            [0, 2 / (top - bottom), 0, -(top + bottom) / (top - bottom)],
+            [0, 0, -2 / (far - near), -(far + near) / (far - near)],
+            [0, 0, 0, 1],
+        ],
+        dtype=np.float32,
+    )
+
+
 class OpenGLWindow:
     def __init__(self):
         self.triangle = None
